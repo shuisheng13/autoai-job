405639ab2a50dea0c134e67de97965084135a7d3
yanghh <136158344@qq.com>
2019-08-05 17:59:19 +0800

    test

---
+++ src/main/java/com/navinfo/autoai/autoaijob/AutoaiJobApplication.java
@@ -0,0 +1,13 @@
+package com.navinfo.autoai.autoaijob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoaiJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoaiJobApplication.class, args);
+    }
+
+}
